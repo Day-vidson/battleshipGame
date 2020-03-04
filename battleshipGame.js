88c5d57fa0eb5a438609d438e6e61b67d828d53c
dawidLaptop <dawifd54@gmail.com>
2020-03-04 18:13:41 +0100

    dawid

--- battleshipGame.js
+++ battleshipGame.js
@@ -53,6 +53,66 @@ var model = {
     }
 }
 
+var controller = {
+    guesses: 0,
+
+    processGuess: function(guess) {
+        var location = parseGuess(guess)
+        if(location) {
+            this.guesses++
+            var hit = model.fire(location)
+            if(hit && model.shipsSunk === model.numShips) {
+                view.displayMessage("Zatopiłeś wszytskie moje okręty, w " + this.guesses + " próbach.")
+            }
+        }
+    }
+}
+
+function parseGuess(guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"]
+
+    if(guess === null || guess.length !== 2) {
+        alert("Ups, proszę wpisać literę i cyfrę.")
+    }
+    else {
+        firstChar = guess.charAt(0)
+        var row = alphabet.indexOf(firstChar)
+        var column = guess.charAt(1)
+
+        if(isNaN(row) || isNaN(column)) {
+            alert("Ups, to nie są współrzędne.")
+        }
+        else if(row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize)  {
+
+        }
+        else {
+            return row + column
+        }
+    }
+    return null
+}
+
+controller.processGuess("A0")
+
+
+controller.processGuess("A6")
+controller.processGuess("B6")
+controller.processGuess("C6")
+
+controller.processGuess("C4")
+controller.processGuess("D4")
+controller.processGuess("E4")
+
+controller.processGuess("B0")
+controller.processGuess("B1")
+controller.processGuess("B2")
+
+// console.log(parseGuess("A0"))
+// console.log(parseGuess("B6"))
+// console.log(parseGuess("G3"))
+// console.log(parseGuess("H0"))
+// console.log(parseGuess("A7"))
+
 // model.fire("06"); // hit
 // model.fire("16"); // hit
 // model.fire("26"); // hit
