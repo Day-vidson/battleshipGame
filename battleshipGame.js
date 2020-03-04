6dc2b2df64bfe7d993ba94312b91862d30807bac
dawidLaptop <dawifd54@gmail.com>
2020-03-04 19:26:49 +0100

    endgame

--- battleshipGame.js
+++ battleshipGame.js
@@ -19,11 +19,10 @@ var model = {
     shipLength: 3,
     shipsSunk: 0,
 
-    ships: [
-        {locations: ["06", "16", "26"], hits: ["", "", ""]},
-        {locations: ["24", "34", "44"], hits: ["", "", ""]},
-        {locations: ["10", "11", "12"], hits: ["", "", ""]}
-    ],
+    ships: [{locations: [0, 0, 0], hits: ["", "", ""]},
+            {locations: [0, 0, 0], hits: ["", "", ""]},
+            {locations: [0, 0, 0], hits: ["", "", ""]}],
+
     fire: function(guess) {
         for(var i = 0; i < this.numShips; i++) {
             var ship = this.ships[i]
@@ -43,6 +42,7 @@ var model = {
         view.displayMessage("Spudłowałeś.")
         return false
     },
+
     isSunk: function(ship) {
         for(var i = 0; i < this.shipLength; i++) {
             if(ship.hits[i] !== "hit") {
@@ -50,6 +50,59 @@ var model = {
             }
             return true
         }
+    },
+
+    generateShipLocations: function() {
+        var locations
+        for(var i = 0; i < this.numShips; i++) {
+            do {
+                locations = this.generateShip()
+            } while(this.collision(locations)) {
+                this.ships[i].locations = locations
+            }
+        }
+    },
+
+    generateShip: function() {
+        var direction = Math.floor(Math.random() * 2)
+        var row, col
+
+        if(direction === 1) {
+            //poziom, pierwsze pole
+            row = Math.floor(Math.random() * this.boardSize)
+            col = Math.floor(Math.random() * (this.boardSize - this.shipLength))
+        }
+        else {
+            //pion, pierwsze pole
+            row = Math.floor(Math.random() * (this.boardSize - this.shipLength))
+            col = Math.floor(Math.random() * this.boardSize)
+        }
+
+        var newShipLocations = []
+        for(var i = 0; i < this.shipLength; i++) {
+            if(direction === 1) {
+                //poziom, następne pola
+                newShipLocations.push(row + "" + (col + i))
+            }
+            else {
+                //pion, następne pola
+                newShipLocations.push((row + i) + "" + col)
+            }
+        }
+        return newShipLocations
+    },
+
+    collision: function(locations) {
+        for(var i = 0; i < this.numShips; i++) {
+            var ship = model.ships[i]
+
+            for(var j = 0; j < locations.length; j++) {
+                if(ship.locations.indexOf(locations[j]) >= 0) {
+                    return true
+                }
+            }
+        }
+        return false
     }
 }
 
@@ -103,7 +156,10 @@ function handleFireButton() {
 function init() {
     var fireButton = document.getElementById("fireButton")
     fireButton.onclick = handleFireButton
+    var guessInput = document.getElementById("guessInput")
     guessInput.onkeypress = handleKeyPress
+
+    model.generateShipLocations()
 }
 
 function handleKeyPress(e) {
